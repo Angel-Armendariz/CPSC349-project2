34d198a89eed13cb948634d1f447fd8dd9e2a5ef
Angel Armendariz <angela126509@gmail.com>
2022-12-12 21:23:39 -0800

    deleted remake stuff

--- Project2/_remake/client-side/get-save-data/index.js
+++
@@ -1,1 +0,0 @@
-export { getSaveData } from "./getSaveData";
--- Project2/node_modules/mailgun.js/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Client from './lib/client';
-import Options from './lib/interfaces/Options';
-import IFormData from './lib/interfaces/IFormData';
-declare class Mailgun {
-    private formData;
-    constructor(FormData: new (...args: any[]) => IFormData);
-    client(options: Options): Client;
-}
-export = Mailgun;
