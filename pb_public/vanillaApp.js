d9fc31457cf009a86753218bd599004934faaf55
unasir335 <umarnasir335@gmail.com>
2022-12-17 17:48:13 -0800

    added memo feature

--- pb_public/vanillaApp.js
+++ pb_public/vanillaApp.js
@@ -14,6 +14,12 @@ const TodoFriday = document.getElementById("TodoFriday");
 const NewTodoFriday = document.getElementById("NewTodoFriday");
 const TodoSaturday = document.getElementById("TodoSaturday");
 const NewTodoSaturday = document.getElementById("NewTodoSaturday");
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+const todoInput = document.getElementById('todo-input')
+
 //adds a todo to the sunday list
 TodoSunday.addEventListener('click', addTodoSunday);
 function addTodoSunday() {
@@ -172,4 +178,53 @@ function addTodoSaturday() {
 title.addEventListener('input', updateValue);
 function updateValue(e) {
   user.innerHTML = e.target.value + "\'s Calendar";
+}
+
+
+
+const todoTemplate = (todoTitle) => `
+  <div class="todo-container text-gray-700 text-center mt-2">
+    <div class="flex justify-between bg-yellow-100 shadow mb-2">
+      <span class="w-full text-left leading-loose p-2 pl-10 cursor-pointer" onClick="checkTodo(this)">${todoTitle}</span>
+      <div class="close-btn p-2 pr-8 text-xl cursor-pointer"></div>
+    </div>
+  </div>
+`
+const newTodo = () => {
+  if (! todoInput.checkValidity()) {
+    alert("Memo Content Empty")
+    
+    return false
+  }
+
+  list.insertAdjacentHTML('beforeend', todoTemplate(todoInput.value))
+  list.lastElementChild.querySelector('.close-btn').addEventListener('click', deleteTodo)
+
+  itemCountSpan.innerHTML++
+  uncheckedCountSpan.innerHTML++
+
+  todoInput.value = ""
+}
+
+
+const checkTodo = (el) => {
+  el.classList.toggle('checked')
+
+  if (el.classList.contains('checked')) {
+    uncheckedCountSpan.innerHTML--
+  } else {
+    uncheckedCountSpan.innerHTML++
+  }
+}
+
+const deleteTodo = (el) => {  
+  const todoContainer = el.target.parentElement
+
+  todoContainer.parentNode.removeChild(el.target.parentElement)
+
+  if (! todoContainer.querySelector('.checked')) {
+    uncheckedCountSpan.innerHTML--
+  }
+
+  itemCountSpan.innerHTML--
 }
